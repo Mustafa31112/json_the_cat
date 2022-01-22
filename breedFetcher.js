04f431ba0a51dd67a62ea17c1352dc399438d033
Mustafa <mustafa31112@gmail.com>
2022-01-22 00:55:20 +0000

    refractored some code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,29 @@
 const request = require('request');
+const fetchBreedDescription = function(breedName, callback) {
+  const catApiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
 
-const breedNameInput = process.argv[2]
-const catApiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedNameInput}`
-
-
-
-
-
-
-
-request.get(catApiUrl, function (error, response, body) {
-  if (error) {
-    return console.log('failed to request breed', error)
-  }
-
-  const data = JSON.parse(body)
-  const breed = data[0];
-  if (!breed) {
-    return console.log('failed to find breed', breedNameInput)
-  }
-
-  return console.log(breed.description);
+  request(catApiUrl, (error, response, body) => {
+    if (error) {
+      return callback(error, null)
+    }
   
-  // console.error('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
-});
+    const data = JSON.parse(body)
+    const breed = data[0];
+    if (breed) {
+       callback(null, breed.description);
+    } else {
+      callback("breed not found");
+    }
+  
+    return (breed.description);
+    
+  });
+  
+};
+
+module.exports = { fetchBreedDescription }
+
+
+// console.error('error:', error); // Print the error if one occurred
+// console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+// console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
